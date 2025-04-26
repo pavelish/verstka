4a5ae0beb2f5606290d1ebfb53c8402ece00b674
pavelish <91222658+pavelish@users.noreply.github.com>
2025-04-26 20:41:58 +0300

    Add files via upload

--- course_work/levels/third/js/script.js
+++ course_work/levels/third/js/script.js
@@ -1,29 +1,35 @@
-function getRandomQuestion() {
-    const red = parseInt(Math.random() * 6);
-    const blue = parseInt(Math.random() * 6);
-    return [red, blue];
-}
+const colors = ['red', 'green', 'blue'];
 function getRandomTextQuestion() {
     let el = document.getElementById('l3-question');
-    txt = `выбирете ${blue} синих матрешек и ${red} красных`;
-    el.appendChild(document.createTextNode(txt));
+    let color = colors[getRandomInt(3)];
+    let num_clk = getRandomInt(20, 5);
+    el.appendChild(document.createTextNode(`Поймайте ${num_clk} матрешек`));
+    console.log(color);
+    el.style.color = color;
+    return [color, num_clk];
+}
+function getRandomInt(max, min = 0) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
 
 function generator() {
-    let number = parseInt(Math.random() * 6);
+    let number = getRandomInt(9);
     let container = window.document.getElementById('l3-container');
     let frame_rect = container.getBoundingClientRect();
     let mtr = document.createElement('div');
     mtr.classList.add('mtr-container');
-    if (number > 2) {
+    if (number > 5) {
+        mtr.classList.add('green');
+    }
+    else if (number > 2) {
         mtr.classList.add('blue');
     }
     else {
         mtr.classList.add('red');
     }
-    mtr.style.left = `${getRandomInt(frame_rect.width + frame_rect.left - 250, frame_rect.left)}px`;
-    mtr.style.top = `${getRandomInt(frame_rect.height + frame_rect.top - 250, frame_rect.top)}px`;
-    mtr.addEventListener('click', (event) => count(event));
+    mtr.style.left = `${getRandomInt(frame_rect.width - 80, 0)}px`;
+    mtr.style.top = `${getRandomInt(frame_rect.height - 80, 0)}px`;
+    mtr.addEventListener('dblclick', (event) => count(event));
     let img = document.createElement('img');
     img.setAttribute('src', matryoshkalist[number]['img']);
     img.classList.add('blink');
@@ -33,16 +39,25 @@ function generator() {
 }
 
 function count(event) {
-    if (event.target.classList.contains('blue')) {
-        blue--;
+    if (event.target.classList.contains(color)) {
+        num_clk--;
+        event.target.style.display = 'none';
+        if (num_clk === 0) {
+            on_end();
+        }
     }
     else {
-        red--;
+        num_clk += 3;
+        event.target.classList.add('wrong-clicked');
+
     }
+    let el = document.getElementById('l3-question')
+    el.innerHTML = '';
+    el.appendChild(document.createTextNode(`Поймайте ${num_clk} матрешек`))
 }
 
 function check_result() {
-    if (blue == 0 && red == 0) {
+    if (num_clk === 0) {
         return true;
     }
     else {
@@ -64,18 +79,20 @@ function make_timer() {
 }
 
 function start_func() {
-
+    let cnt = document.getElementById('l3-container');
+    cnt.classList.remove('selected-green');
+    cnt.classList.remove('selected-red');
     checkButton.disabled = false;
-    let tmp = getRandomQuestion();
     let el_q = document.getElementById('l3-question');
     el_q.innerHTML = '';
+    let tmp = getRandomTextQuestion();
     el_q = document.getElementById('l3-result');
     el_q.innerHTML = '';
-    blue = tmp[0];
-    red = tmp[1];
-    getRandomTextQuestion();
+    color = tmp[0];
+    num_clk = tmp[1];
     time = 38 - level * 5 > 0 ? 38 - level * 5 : 8;
-    setInterval(generator, 3000 - level * 1000);
+    console.log(color);
+    intrv = setInterval(generator, 1000);
 }
 
 function getRandomInt(max, min = 0) {
@@ -85,7 +102,9 @@ function on_end() {
     checkButton.disabled = true;
     let result_msg = document.getElementById('l3-result');
     let txt;
+    let cnt = document.getElementById('l3-container');
     if (check_result()) {
+        cnt.classList.add('selected-green');
         txt = document.createTextNode('Молодец, ты победил');
         let rating = JSON.parse(localStorage.getItem('rating'));
         rating.find((item) => item.name == localStorage.getItem('user'))['scores'] += 3 * (level + 1);
@@ -98,8 +117,11 @@ function on_end() {
         }
     }
     else {
+        cnt.classList.add('selected-red');
         txt = document.createTextNode('К сожалению, ты проиграл, давай попробуем сначала');
     }
+    cnt.innerHTML = '';
+    clearInterval(intrv);
     result_msg.appendChild(txt);
     setTimeout(start_func, 4000);
 }
@@ -107,10 +129,11 @@ function finish() {
     document.location = '../../index.html';
 }
 
+let intrv
 let level = 0;
 let time;
-let blue;
-let red;
+let color;
+let num_clk;
 
 let checkButton = document.getElementById('checkLevel3Btn');
 checkButton.addEventListener('click', on_end);
